3ee7448547a9317890878b80bbdbba11f50b50b5
User <User@LAB2-01>
2024-10-30 10:30:01 +0200

    ...........

--- ViewModels/LoginPageViewModel.cs
+++ ViewModels/LoginPageViewModel.cs
@@ -1,9 +1,41 @@
+using System.Windows.Input;
+
 namespace BSClient.ViewModels;
 
 public class LoginPageViewModel : ContentPage
 {
-	public LoginPageViewModel()
-	{
-		
-	}
+	
+
+    public ICommand LoginCommand { get; set; }
+    public ICommand GoToRegisterCommand { get; set; }
+    private string email;
+    public string Email
+    {
+        get => email;
+        set
+        {
+            if (email != value)
+            {
+                email = value;
+                OnPropertyChanged(nameof(Email));
+                // can add more logic here like email validation etc.
+                // can add error message property and set it here
+            }
+        }
+    }
+    private string password;
+    public string Password
+    {
+        get => password;
+        set
+        {
+            if (password != value)
+            {
+                password = value;
+                OnPropertyChanged(nameof(Password));
+                // can add more logic here like email validation etc.
+                // can add error message property and set it here
+            }
+        }
+    }
 }
---
+++ Views/Register.xaml.cs
@@ -0,0 +1,9 @@
+namespace BSClient.Views;
+
+public partial class Register : ContentPage
+{
+	public Register()
+	{
+		InitializeComponent();
+	}
+}
